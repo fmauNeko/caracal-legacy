9743c28007961f17ceb744781ad6b7041447b0a2
fmauNeko <mail@fmauneko.eu>
2009-08-19 20:00:42 +0200

    Customisation des aperçus :D

---
+++ www/highslide/highslide.cfg.js
@@ -0,0 +1,4 @@
+hs.outlineType = 'rounded-black';
+hs.wrapperClassName = 'borderless floating-caption';
+hs.dimmingOpacity = 0.75;
+hs.align = 'center';
--- www/template.php
+++ www/template.php
@@ -9,6 +9,7 @@
 	<link href="highslide/highslide-ie6.css" media="screen" rel="Stylesheet" type="text/css" />
 	<![endif]-->
 	<script type="text/javascript" src="highslide/highslide.js"></script>
+	<script type="text/javascript" src="highslide/highslide.cfg.js"></script>
 	<link rel="alternate" title="Flux rss" type="application/rss+xml" href="rss.php" />
 	<title><?php echo $titre_site; ?></title>
 </head>
